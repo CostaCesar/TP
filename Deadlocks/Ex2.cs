a376ffd415eb7c2a2c3ed76162db442b62caca49
CostaCesar <2021951620@teiacoltec.org>
2023-03-10 08:52:15 -0300

    Better worker in ex2

--- Deadlocks/Ex2.cs
+++ Deadlocks/Ex2.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Threading;
 
-public class Deadlock_Ex2 : Exec
+public class A3X2 : Exec
 {
     internal class Worker
     {
@@ -27,22 +27,26 @@ public class Deadlock_Ex2 : Exec
                 Console.WriteLine(i + " cycle:");
                 
                 try
-                { Thread.Sleep(rng.Next(time * 1000)); }
+                { Thread.Sleep(rng.Next(time * 500)); }
                 catch(ThreadInterruptedException e)
                 { Console.WriteLine("Stopped!" + e); }
                 
                 Console.WriteLine("Finished " + product);
             }
+            Console.WriteLine("The assembly line of " + this.product + " ended!");
         }
     }
     
     public override void Execute()
     {
         Worker alex = new Worker("Shoe", 4, 50);
-        Worker john = new Worker("Shoe", 2, 75);
+        Worker john = new Worker("Paper", 2, 75);
         Worker mike = new Worker("Box", 1, 150);
         alex.thread.Start();
         mike.thread.Start();
         john.thread.Start();
+        alex.thread.Join();
+        mike.thread.Join();
+        john.thread.Join();
     }
 }
